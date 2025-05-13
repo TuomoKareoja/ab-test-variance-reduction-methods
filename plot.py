6fa7f5bc1098157cf79b9867195d7d3b78ebb55e
TuomoKareoja <tuomo.kareoja@gmail.com>
2025-05-13 19:33:11 +0200

    add basic plotting

--- plot.py
+++ plot.py
@@ -5,8 +5,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import logging
-import numpy as np
-from dvc import api
 
 # %%
 
@@ -18,5 +16,27 @@ logger.info("Starting plotting script")
 # %%
 
 # For each experiment variation load the results and plot them saving the outcome to plots folder
+df_post_only = pd.read_parquet(os.path.join("results", "post_only.parquet"))
+df_pre_and_post = pd.read_parquet(os.path.join("results", "pre_and_post.parquet"))
+
+# %%
+
+for df in [df_post_only, df_pre_and_post]:
+    df["estimate_error"] = df["estimate"] - df["true_effect"]
+
+# %%
+
+os.makedirs("plots", exist_ok=True)
+
+for df in [df_post_only, df_pre_and_post]:
+
+    # Create the boxplot
+    plt.figure(figsize=(10, 6))
+    sns.boxplot(data=df, x="estimate_error", y="method", orient="h")
+    plt.title("Estimate Error by Method")
+    plt.xlabel("Estimate Error")
+    plt.ylabel("Method")
+
+    plt.show()
 
 # %%
